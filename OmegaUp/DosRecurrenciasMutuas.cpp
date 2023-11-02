2a453260e0bd6c2bb506e6c21f7850cd1878257a
Daniel Valido <jvalidojeronimo@gmail.com>
2023-11-02 00:54:14 -0600

    Second Problem

---
+++ OmegaUp/DosRecurrenciasMutuas.cpp
@@ -0,0 +1,22 @@
+//https://omegaup.com/arena/problem/Dos-recurrencias-mutuas/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+int main(){
+    std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+    freopen("../input.in","r",stdin);
+    freopen("../output.out","w",stdout);
+
+    int n;
+
+    std::cin>>n;
+
+    std::cout<<n/2;
+
+    return 0;
+}
